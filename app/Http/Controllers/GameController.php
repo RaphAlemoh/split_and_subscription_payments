8c67f83997f88bc28c2ed99de7281abc0868c13a
Alemoh Rapheal <raphealenike@gmail.com>
2021-08-31 06:27:22 -0700

    Add Display based on subscrption

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Game;
-use Illuminate\Http\Request;
+use App\Models\Subscription;
+use Illuminate\Support\Facades\Auth;
 
 class GameController extends Controller
 {
     public function games(){
 
-        $games = Game::all();
-        
+        $user = Auth::user();
+        $subscribtion_status = Subscription::where(['user_id' => $user->id, 'status' => 1])->get();
+        $games = count($subscribtion_status) > 0 ? Game::all() : Game::where('amount', '0')->get();
         return view('games.index')->with(compact('games'));
     }
 }
